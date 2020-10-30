f536312bd3e5ed49387267cb568e5d5f35934791
sawan848 <53111074+sawan848@users.noreply.github.com>
2020-10-30 12:20:23 +0530

    Update MultThreadNewChanges.java

--- multithreading/MultThreadNewChanges.java
+++ multithreading/MultThreadNewChanges.java
@@ -1,1 +1,27 @@
-package multithreading;import java.util.concurrent.CompletableFuture;import java.util.concurrent.ExecutionException;import static java.lang.Thread.sleep;/* * @author:Saawan * @created:[10/29/2020],Thursday * Time:9:35 PM * completableFuture */public class MultThreadNewChanges {    private static int data=5;    public static void main(String[] args) throws InterruptedException, ExecutionException {        System.out.println("in main thread data");        CompletableFuture.runAsync(()->{            try {              sleep(1500);            } catch (InterruptedException e) {                e.printStackTrace();            }        });        System.out.println("main thread");        System.out.println("data not changes as complete future in progress-->"+data);        Thread.sleep(2500);        System.out.println("data changed by complete future -->"+data);    }}
+package multithreading;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import static java.lang.Thread.sleep;
+/*
+ * @author:Saawan
+ * @created:[10/29/2020],Thursday
+ * Time:9:35 PM
+ * completableFuture
+ */
+public class MultThreadNewChanges {
+    private static int data=5;
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        System.out.println("in main thread data");
+        CompletableFuture.runAsync(()->{
+            try {
+              sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println("main thread");
+        System.out.println("data not changes as complete future in progress-->"+data);
+        Thread.sleep(2500);
+        System.out.println("data changed by complete future -->"+data);
+    }
+}
