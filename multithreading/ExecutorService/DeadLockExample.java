9e4a40fe623ab4595d6e20271424ce4764c91f30
sawan848 <53111074+sawan848@users.noreply.github.com>
2020-11-03 17:41:29 +0530

    Update DeadLockExample.java

--- multithreading/ExecutorService/DeadLockExample.java
+++ multithreading/ExecutorService/DeadLockExample.java
@@ -1,1 +1,47 @@
-package ExecutorService;/* * @author:Saawan * @created:[11/2/2020],Monday * Time:5:56 PM */public class DeadLockExample {    public static void main(String[] args) {        final String lock1="Employee";        final String lock2="EmployeeData";        Thread thread=new Thread( new Runnable() {            public void run() {                synchronized (lock1) {                    System.out.println("inside thread 1");                try {                    Thread.sleep(500);                } catch (InterruptedException e) {                    e.printStackTrace();                }                synchronized (lock2){                    System.out.println("lock 1 executed properly ");                }                }            }        });        Thread thread1=new Thread( new Runnable() {            public void run() {                synchronized (lock1) {                    System.out.println("inside thread 2");                        try {                            Thread.sleep(500);                        } catch (InterruptedException e) {                            e.printStackTrace();                        }                synchronized (lock2){                    System.out.println("lock 2 executed properly ");                }            }            }        });        thread.start();        thread1.start();    }}
+package ExecutorService;
+/*
+ * @author:Saawan
+ * @created:[11/2/2020],Monday
+ * Time:5:56 PM
+ */
+public class DeadLockExample {
+    public static void main(String[] args) {
+        final String lock1="Employee";
+        final String lock2="EmployeeData";
+
+        Thread thread=new Thread( new Runnable() {
+            public void run() {
+                synchronized (lock1) {
+                    System.out.println("inside thread 1");
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2){
+                    System.out.println("lock 1 executed properly ");
+                }
+                }
+            }
+        });
+        Thread thread1=new Thread( new Runnable() {
+            public void run() {
+                synchronized (lock1) {
+                    System.out.println("inside thread 2");
+
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                synchronized (lock2){
+                    System.out.println("lock 2 executed properly ");
+                }
+            }
+            }
+        });
+        thread.start();
+        thread1.start();
+    }
+}
