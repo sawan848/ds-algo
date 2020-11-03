3bb219f95f1d214c6d32858fab33cf0f7004d9e0
sawan848 <53111074+sawan848@users.noreply.github.com>
2020-11-03 17:44:23 +0530

    Update ExecutorServiceUtilities.java

--- multithreading/ExecutorService/ExecutorServiceUtilities.java
+++ multithreading/ExecutorService/ExecutorServiceUtilities.java
@@ -1,1 +1,32 @@
-package ExecutorService;/* * @author:Saawan * @created:[11/1/2020],Sunday * Time:8:32 PM */import java.util.ArrayList;import java.util.Date;import java.util.List;import java.util.concurrent.*;public class ExecutorServiceUtilities {    public static void main(String[] args) throws InterruptedException, ExecutionException {        ExecutorService es= Executors.newFixedThreadPool(2);        System.out.println(new Date());        List<Future<String>>futureList=new ArrayList<>();        for (int i = 0; i < 10; i++) {            futureList.add((Future<String>)es.submit(new Service(i)));        }        es.shutdown();        es.awaitTermination(10, TimeUnit.SECONDS);        System.out.println("--> --> --> -->  -->=== === === === === --> --> --> --> -->");        for (Future<String>fut:futureList)            System.out.println(fut.get());        System.out.println(new Date());    }}
+package ExecutorService;
+/*
+ * @author:Saawan
+ * @created:[11/1/2020],Sunday
+ * Time:8:32 PM
+ */
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExecutorServiceUtilities {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService es= Executors.newFixedThreadPool(2);
+        System.out.println(new Date());
+
+        List<Future<String>>futureList=new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            futureList.add((Future<String>)es.submit(new Service(i)));
+        }
+        es.shutdown();
+        es.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println("--> --> --> -->  -->=== === === === === --> --> --> --> -->");
+        for (Future<String>fut:futureList)
+            System.out.println(fut.get());
+        System.out.println(new Date());
+
+
+
+    }
+}
