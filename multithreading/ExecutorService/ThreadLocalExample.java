ac3f56559b2162a54a8bfb9bf2efe6443e4f0a2d
sawan848 <53111074+sawan848@users.noreply.github.com>
2020-11-03 17:44:42 +0530

    Update ThreadLocalExample.java

--- multithreading/ExecutorService/ThreadLocalExample.java
+++ multithreading/ExecutorService/ThreadLocalExample.java
@@ -1,1 +1,16 @@
-package ExecutorService;/* * @author:Saawan * @created:[11/1/2020],Sunday * Time:9:48 PM */public class ThreadLocalExample {    public static void main(String[] args) {        ThreadLocal<String>t1=new ThreadLocal<String>(){           protected String initialValue(){ return "Play java return";}        };        System.out.println("g"+t1.get());        t1.set("Play java");        System.out.println(t1.get());    }}
+package ExecutorService;
+/*
+ * @author:Saawan
+ * @created:[11/1/2020],Sunday
+ * Time:9:48 PM
+ */
+public class ThreadLocalExample {
+    public static void main(String[] args) {
+        ThreadLocal<String>t1=new ThreadLocal<String>(){
+           protected String initialValue(){ return "Play java return";}
+        };
+        System.out.println("g"+t1.get());
+        t1.set("Play java");
+        System.out.println(t1.get());
+    }
+}
